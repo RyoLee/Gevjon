6aec502eb15c15158951e7f08c3dd48b3149b110
RyoLee <516127941@qq.com>
2022-06-07 05:07:45 +0800

    fix: typo

--- Gevjon-Core/Properties/AssemblyInfo.cs
+++ Gevjon-Core/Properties/AssemblyInfo.cs
@@ -28,7 +28,6 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
